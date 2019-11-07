c28198e1b54cc8c8cf727a9046ec4ee9265bc4e0
maliecology <mlubic@vols.utk.edu>
2019-11-07 11:13:01 -0500

    Editing initial comments

--- final_proj_v1.R
+++ final_proj_v1.R
@@ -1,14 +1,17 @@
 ##################
-#Oldies But Goodies Final Project
-#Using TensorFlow to build a package that performs simple shape image identification
+# Oldies But Goodies Final Project
+# Using TensorFlow to build a package that performs simple shape image identification
 ##################
 
-#Loading in Keras that can load in TensorFlow
+# Loading in Keras that can load in TensorFlow
 install.packages("devtools", lib = "~/R_lib/")
 library(devtools)
 devtools::install_github("rstudio/keras", lib = "~/R_lib/")
 library(keras)
 install_keras()
 
-#Loading in Fashion MNIST dataset
+# Loading in Fashion MNIST dataset
 fashion_mnist <- dataset_fashion_mnist()
+
+
+# 
