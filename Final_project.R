a00900288d66f0814f965067bb9e9aa95e6f7cd5
lxrader <lrader2@utk.edu>
2019-11-15 10:42:53 -0500

    Added the code from canny_edge_test.R to this code to have 1 .R file and commented the steps

--- Final_project.R
+++ Final_project.R
@@ -1,7 +1,9 @@
-######################
+#######################################
 #Final Project Code
-#Using Keras and Tensorflow
-######################
+#Due completely on December 2, 2019
+#Oldies but Goodies
+#A program that takes a dataset of images and can classify the image, simplify them to edges, then identify the lines visible
+######################################
 
 
 #Installing packages
@@ -17,6 +19,12 @@ install.packages("FixedPoint")
 library(FixedPoint)
 install.packages("purrr")
 library(purrr)
+
+
+#######################
+#Classification Step
+#######################
+
 
 #Defining the data
 #myData <- dataset_mnist()
@@ -84,3 +92,81 @@ network %>% fit(train_images, train_labels, epochs = 5, batch_size = 128)
 #Outputting the metrics
 metrics <- network %>% evaluate(test_images, test_labels)
 metrics
+
+
+
+
+#######################
+#Edge Detection Layer
+#######################
+
+#Using a gaussian filter to denoise
+im <- grayscale(boats) %>% isoblur(2)
+
+#Computing an image gradient
+gr <- imgradient(im,"xy")
+plot(gr,layout="row")
+
+#Computing the gradient magnitude
+mag <- with(gr,sqrt(x^2+y^2))
+plot(mag)
+
+#Determining the local orientation with the gradient angle
+ang <- with(gr,atan2(y,x))
+plot(ang)
+
+#Simplifying the image using non-maxima thresholding
+threshold(mag) %>% plot
+
+#Going along the (normalised) gradient
+#Xc(im) is an image containing the x coordinates of the image
+nX <- Xc(im) + gr$x/mag 
+nY <- Yc(im) + gr$y/mag
+#nX and nY are not integer values, so we can't use them directly as indices.
+#We can use interpolation, though:
+val.fwd <- interp(mag,data.frame(x=as.vector(nX),y=as.vector(nY)))
+
+nX <- Xc(im) - gr$x/mag 
+nY <- Yc(im) - gr$y/mag
+val.bwd <- interp(mag,data.frame(x=as.vector(nX),y=as.vector(nY)))
+
+throw <- (mag < val.bwd) | (mag < val.fwd)
+mag[throw] <- 0
+plot(mag)
+
+#Identifying strong and weak thresholds for classifying edge pixels
+#strong threshold
+t2 <- quantile(mag,.96)
+#weak threshold 
+t1 <- quantile(mag,.90)
+layout(t(1:2))
+
+strong <- mag>t2
+plot(strong,main="Initial set of strong edges")
+weak <- mag %inr% c(t1,t2)
+plot(weak,main="Initial set of weak edges")
+
+#Final step is producing the image called strong.new that is strong edges only version of OG image
+overlap <- grow(strong,3) & weak 
+strong.new <- strong | overlap
+plot(strong.new,main="New set of strong edges")
+
+#Converting strong.new from a pixel image to dataframe
+as.data.frame(strong.new)
+
+
+
+#Blurring the boats image
+im.blurry <- isoblur(boats, 10)
+
+
+
+
+
+
+##########################
+#Hough transform
+##########################
+
+boat_line <- hough_line(strong.new, ntheta = 500, data.frame = TRUE, shift = FALSE) %>% plot
+#boat_line <- hough_line(grayscale(im.blurry), ntheta = 500, data.frame = FALSE, shift = TRUE) %>% plot
